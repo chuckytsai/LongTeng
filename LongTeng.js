e6318d52e49c063589810131a41631253d7f51dc
chuckytsai <64692566+chuckytsai@users.noreply.github.com>
2021-09-14 21:45:33 +0800

    2021/09/14
    變更首頁 新增圖片檔  變更index名稱

--- LongTeng.js
+++ LongTeng.js
@@ -10,21 +10,29 @@ localStorage.setItem("RainbowBallDeg", RainbowBallDeg);
 function frist() {
     localStorage.setItem("RainbowBallDeg", parseInt(localStorage.RainbowBallDeg) + 2);
     RainbowBall.style.transform = "rotate(" + localStorage.RainbowBallDeg + "deg)";
-    setTimeout("second()", 100);
+    // RainbowBall.style.left = +localStorage.RainbowBallDeg + 1 + "px";
+    // if (localStorage.RainbowBallDeg > 2490) {
+    //     localStorage.setItem("RainbowBallDeg", 0);
+    //     setTimeout("second()", 100);
+    // }
+    // else {
+    //     setTimeout("second()", 100);
+    // }
 }
 
 function second() {
     localStorage.setItem("RainbowBallDeg", parseInt(localStorage.RainbowBallDeg) + 2);
     RainbowBall.style.transform = "rotate(" + localStorage.RainbowBallDeg + "deg)";
+    // RainbowBall.style.left = +localStorage.RainbowBallDeg + 1 + "px";
     setTimeout("third()", 100);
 }
 
 function third() {
     localStorage.setItem("RainbowBallDeg", parseInt(localStorage.RainbowBallDeg) + 2);
     RainbowBall.style.transform = "rotate(" + localStorage.RainbowBallDeg + "deg)";
+    // RainbowBall.style.left = +localStorage.RainbowBallDeg + 1 + "px";
     setTimeout("frist()", 100);
 }
-
 
 
 window.addEventListener("scroll", () => {
